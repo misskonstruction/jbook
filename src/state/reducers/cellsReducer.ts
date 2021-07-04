fdd6dce2cff8c6cfac919daaadf0dbb6e01eea54
misskonstruction <misskonstruction@gmail.com>
2021-07-04 08:56:58 -0400

    implemented immer for direct updates to state

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {ActionType} from '../action-types';
 import {Action} from '../actions';
 import {Cell} from '../cell';
@@ -18,23 +19,15 @@ const initialState: CellsState = {
     data: {},
 };
 
-const reducer = (
+const reducer = produce((
     state: CellsState = initialState, 
     action: Action
-): CellsState => {
+) => {
     switch (action.type){
         case ActionType.UPDATE_CELL:
-            const {id, content} = action.payload;
-            return {
-                ...state,
-                data: {
-                    ...state.data, 
-                    [id]: {
-                        ...state.data[id],
-                        content,
-                    },
-                },
-            };
+            const {id, content} = action.payload; 
+            state.data[id].content = content;
+            return;
         case ActionType.DELETE_CELL:
             return state;
         case ActionType.INSERT_CELL_BEFORE:
@@ -45,6 +38,6 @@ const reducer = (
             return state;
 
     }
-};
+});
 
 export default reducer;
