f42a19749aa2cb44d2c94fef4c6067592838eec8
misskonstruction <misskonstruction@gmail.com>
2021-07-18 18:36:03 -0400

    added some styling

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,3 +1,4 @@
+import './action-bar.css';
 import {useActions} from '../hooks/use-actions';
 
 interface ActionBarProps {
@@ -6,7 +7,7 @@ interface ActionBarProps {
 
 const ActionBar: React.FC<ActionBarProps> = ({id}) => {
     const {moveCell, deleteCell} = useActions();
-    return( <div>
+    return( <div className="action-bar">
         <button className="button is-primary is-small" onClick={() => moveCell(id, 'up')}>
             <span className="icon">
                <i className="fas fa-arrow-up"></i> 
