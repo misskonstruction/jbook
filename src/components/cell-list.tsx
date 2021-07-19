2c49c20ea3448dac07fb6ece09b2c12cef029f4b
misskonstruction <misskonstruction@gmail.com>
2021-07-19 09:09:04 -0400

    styled AddCell and forced visibility for options when all cells deleted

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import {Fragment} from 'react';
 import {useTypedSelector} from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
@@ -8,16 +9,16 @@ const CellList: React.FC = () => {
     );
 
     const renderedCells = cells.map(cell => (
-        <>
+        <Fragment key={cell.id}>
         <AddCell nextCellId={cell.id}/>
         <CellListItem key={cell.id} cell={cell}/>
-        </>
+        </Fragment>
     ));
 
     return( 
         <div>
             {renderedCells}
-            <AddCell nextCellId={null} />
+            <AddCell forceVisible={cells.length === 0} nextCellId={null} />
         </div>
     );
     
